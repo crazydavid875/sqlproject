f19c6005c6296269e6978ae3192fc22fcfa12d73
david <crazydavid875@gmail.com>
2021-01-06 16:48:52 +0800

    fix header('Access-Control-Allow-Methods:*');

--- route.php
+++ route.php
@@ -5,7 +5,8 @@ $isManager = false;
 $route = new Router(Request::uri()); //搭配 .htaccess 排除資料夾名稱後解析 URL
 $route->getParameter(1); // 從 http://127.0.0.1/game/aaa/bbb 取得 aaa 字串之意
 header("Access-Control-Allow-Origin:*");
-header('Access-Control-Allow-Methods:GET,PUT,POST,PATCH,DELETE,OPTIONS');
+header('Access-Control-Allow-Methods:*');
+header('Access-Control-Allow-Headers:*');
 // 用參數決定載入某頁並讀取需要的資料
 $isauth = CheckAuth();
 switch($route->getParameter(1)){
