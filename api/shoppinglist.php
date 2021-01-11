ca1829b2cdb90d0b890ed062a3387d2b9adef0ac
david <crazydavid875@gmail.com>
2021-01-11 19:34:26 +0800

    fix shoppinglist

--- api/shoppinglist.php
+++ api/shoppinglist.php
@@ -244,7 +244,10 @@ function Select($id){
     if($id!=''){
         $where = "$table.id = ".$id;
     }
-    $showData = "shoppinglist.*,coupon.*,
+     $showData = "shoppinglist.id,shoppinglist.stateid,shoppinglist.address,
+    shoppinglist.buyDatetime,shoppinglist.memberid,shoppinglist.couponid,
+    shoppinglist.phone,coupon.hash,coupon.context,coupon.discount,coupon.startdate
+    ,coupon.enddate,
     round(COALESCE((sum(game.price*havelist.quantity)*coupon.discount),0)) as 'total'";
     $query = "SELECT  $showData
     FROM shoppinglist 
